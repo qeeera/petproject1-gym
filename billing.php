ee6e5b431e7465cb787e2296dea56107b807817e
qeeera <savitchgarik@yandex.ru>
2023-07-27 23:51:13 +0300

    first

---
+++ billing.php
@@ -0,0 +1,91 @@
+<?php
+
+include 'config.php'; 
+
+if(isset($_REQUEST["submit"])) {
+
+  $id = $_REQUEST["id"];
+  $name = $_REQUEST["name"];
+  $period = $_REQUEST["period"];
+  $amount = $_REQUEST["amount"];
+
+  $ins = "INSERT INTO billing (id,name,period,price ) VALUES('$id', '$name', '$period', '$amount')";
+  
+  $query1 = mysqli_query($connection, $ins);
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+<body>
+    <!-- navbar start  -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="admin-login.php" ><img src="img/TT.png" alt="Tutitions Tonight " srcset=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="billing.php">Gym Managment System <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="billing.php">Billing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="coach.php">Coach</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="members.php">Member</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="receptionist.php">Receptionist</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- navbar end -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+<!-- form start  -->
+
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Billing ID</label>
+      <input type="text" name="id" class="form-control" id="inputEmail4" placeholder="ID">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Name</label>
+      <input type="text" name="name" class="form-control" id="inputPassword4" placeholder="NAME">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Date</label>
+    <input type="text" name="period" class="form-control" id="inputAddress" placeholder="date">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Amount</label>
+    <input type="text" name="amount" class="form-control" id="inputAddress2" placeholder="Amount">
+  </div>
+  
+  <div class="form-group">
+    <!-- <label for="inputAddress2">Button</label> -->
+    <input type="submit" name="submit" class="form-control" id="inputAddress2" Value="Submit">
+  </div>
+  
+    <!-- form end  -->
+</body>
+</html>
